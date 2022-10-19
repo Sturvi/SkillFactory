7f8a615fc178799f0d5bcf55ac5abcdd82f7585f
Emin <114976816+Sturvi@users.noreply.github.com>
2022-10-19 13:57:43 +0400

    Rocket timer code writed

---
+++ Test/src/Main.java
@@ -0,0 +1,21 @@
+
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+
+        for (int i = 10; i >=0; i--) {
+            System.out.println("До запуска ракеты осталось: "+i + " секунд");
+            Thread.sleep(1000);
+
+
+
+        }
+
+        System.out.println("Поехали...");
+        System.out.println("");
+        System.out.println("");
+        Thread.sleep(2000);
+
+        System.out.println("Имформбюро: Юрий Гагарин успешно полетел в космос!");
+    }
+}
