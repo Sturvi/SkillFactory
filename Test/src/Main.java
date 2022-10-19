a9901f72a783ebbdbfd19aef254de16aa24badc0
Emin <114976816+Sturvi@users.noreply.github.com>
2022-10-19 13:58:32 +0400

    Update Main.java

--- Test/src/Main.java
+++ Test/src/Main.java
@@ -3,12 +3,9 @@
 public class Main {
     public static void main(String[] args) throws InterruptedException {
 
-        for (int i = 10; i >=0; i--) {
-            System.out.println("До запуска ракеты осталось: "+i + " секунд");
+        for (int i = 10; i >= 0; i--) {
+            System.out.println("До запуска ракеты осталось: " + i + " секунд");
             Thread.sleep(1000);
-
-
-
         }
 
         System.out.println("Поехали...");
